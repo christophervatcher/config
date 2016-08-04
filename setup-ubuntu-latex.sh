c49c9ebc9c4952f879c2b74a85e922712905f89a
Christopher Vatcher <christopher.vatcher@gmail.com>
2016-08-03 23:00:10 -0400

    Added basic package install scripts for Ubuntu.

---
+++ setup-ubuntu-latex.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This script assumes git and vim are installed.
+apt install texlive latexmk
+
---
+++ setup-ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# This script assumes git and vim are installed.
+apt install zsh zsh-doc
+apt install python python3 python-pip python3-pip
+apt install ssh
+apt install screen
+
